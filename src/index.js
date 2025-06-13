79cc0f7a9ac9940e224d1964dfcde53072f31072
Santosh raj <santoshrajgupta7366@gmail.com>
2025-06-13 19:27:00 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ const { PORT } = require('./config/serverConfig');
 
 // const { sendBasicEmail } = require('./services/email-service');
 
-const cron = require('node-cron');
+const TicketController = require('./controllers/ticket-controller'); 
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
 
     app.use(bodyParser.urlencoded({extended: true}));
+    app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
-
+        jobs();
         // sendBasicEmail(
         //     'support@admin.com',
         //     'rajsantosh1030@gmail.com',
@@ -24,9 +27,7 @@ const setupAndStartServer = () => {
 
         // );
 
-        cron.schedule('*/2 * * * *', () => {
-            console.log('running a task every two minute');
-        });
+        
 
     });
 }
